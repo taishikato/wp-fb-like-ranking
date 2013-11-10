09bcaf564636e0566dfb6eeca1444d1de6db35a2
Taishi Kato <taishi.k0903@gmail.com>
2013-11-10 13:08:23 +0900

    version1.12

--- wp-fb-like-ranking.php
+++ wp-fb-like-ranking.php
@@ -4,7 +4,7 @@ Plugin Name: WP Facebook Like Ranking
 Plugin URI: http://wordpress.org/extend/plugins/wp-facebook-like-ranking/
 Description: you can use a your posts' ranking rated by the number of Facebook like./facebookのいいね数に応じた、ブログ記事のランキングを生成します。
 Author: Taishi Kato
-Version: 1.1
+Version: 1.12
 Author URI: http://taishikato.com/
 */
 
