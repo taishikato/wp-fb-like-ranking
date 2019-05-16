078f415f78fdea3a8a1f91fce9b450855cfc91c4
taishikato <taishi.k0903@gmail.com>
2019-05-16 13:51:49 -0700

    :zap: change structure

---
+++ doc-src/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img src="~assets/buefy.png" alt="Buefy" height="28" />
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav> -->
+
+    <section class="main-content columns">
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
---
+++ doc-src/nuxt.config.js
@@ -0,0 +1,82 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  // buildDir: '../doc',
+  router: {
+    base: '/wp-fb-like-ranking/'
+  },
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/css/bulma', '@/assets/css/main'],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    '@nuxtjs/markdownit'
+  ],
+  markdownit: {
+    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
+    breaks: true, // 改行コードを<br>に変換する
+    html: true, // HTML タグを有効にする
+    linkify: true, // URLに似たテキストをリンクに自動変換する
+    typography: true // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+  },
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
